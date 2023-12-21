a66237b288bb5bc989dd2166b7e5b8b3b2dfec27
varunmauraya2022 <varunmauraya@2022email.com>
2023-12-21 09:29:16 +0530

    update practice2.js

--- practice2.js
+++ practice2.js
@@ -1,15 +1,32 @@
-let score = prompt("Enter your score");
-let grade;
-if (score >= 90 && score <= 100) {
-   grade = "A";
-} else if (score >= 70 && score <= 89) {
-   grade = "B";
-} else if (score >= 60 && score <= 69) {
-   grade = "C";
+// let score = prompt("Enter your score");
+// let grade;
+// if (score >= 90 && score <= 100) {
+//    grade = "A";
+// } else if (score >= 70 && score <= 89) {
+//    grade = "B";
+// } else if (score >= 60 && score <= 69) {
+//    grade = "C";
+// }
+// else if (score >= 50 && score <= 59) {
+//    grade = "D";
+// } else {
+//    grade = "F";
+// }
+// console.log("According to you score , your grade was : ", grade);
+
+let mode = "orange";
+let color;
+
+if (mode === "dark") {
+   color = "Black";
 }
-else if (score >= 50 && score <= 59) {
-   grade = "D";
-} else {
-   grade = "F";
+else if (mode === "pink") {
+   color = "pink";
 }
-console.log("According to you score , your grade was : ", grade);
+else if (mode === "grey") {
+   color = "grey";
+}
+else {
+   color = "white";
+}
+console.log("color = ", color);
