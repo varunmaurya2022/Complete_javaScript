989dc5e915544f03b687f13308fc14e07278a6dc
varunmauraya2022 <varunmauraya@2022email.com>
2023-12-22 19:28:27 +0530

    add find_len.js

---
+++ find_len.js
@@ -0,0 +1,7 @@
+let str = "Hello javascript";
+let count = 0;
+for (let i of str) {
+   count += 1;
+}
+console.log("String is ", str, "Length of ", str, "is ", count);
+document.write("String is :", str, ",", " Length 0f ", str, " is :", count);
