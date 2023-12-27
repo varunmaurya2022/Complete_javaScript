4126bfde90295d2ff2c444c013d53dfd37a6b090
varunmauraya2022 <varunmauraya@2022email.com>
2023-12-27 10:13:26 +0530

    add temp_lit

---
+++ temp_lit.js
@@ -0,0 +1,12 @@
+let prod = "Watch Box";
+let price = 10;
+
+console.log(`The cost of ${prod} is ${price} rupees!`);
+// let b = prompt("Enter your name ");
+// document.write(`Your name is ${b}`);
+// console.log(b);
+
+let fullName = prompt("Enter The fullName");
+let username = "@" + fullName + fullName.length;
+console.log(username);
+document.write(`username is ${username}`);
